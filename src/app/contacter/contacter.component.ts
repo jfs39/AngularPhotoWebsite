6b80ebe2cef09c8330e59dd4189010cd0d79f24a
jfs39 <jfs.serg@hotmail.com>
2020-06-19 11:40:12 -0400

    added components

---
+++ src/app/contacter/contacter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'photo-contacter',
+  templateUrl: './contacter.component.html',
+  styleUrls: ['./contacter.component.css']
+})
+export class ContacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
